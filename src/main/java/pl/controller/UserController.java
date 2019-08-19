a4fc7893c2909963bcaa81d9f43d5810bd54baa1
Sonia Nocon <noconsonia@gmail.com>
2019-08-19 10:48:05 +0200

    user profile

---
+++ src/main/java/pl/controller/UserController.java
@@ -0,0 +1,145 @@
+package pl.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import pl.DTO.PasswordResetDto;
+import pl.entity.ConfirmationToken;
+import pl.entity.User;
+import pl.repository.*;
+import pl.service.EmailSenderService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ConfirmationTokenRepository confirmationTokenRepository;
+
+    @Autowired
+    private EmailSenderService emailSenderService;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
+    @ModelAttribute("passwordResetForm")
+    public PasswordResetDto passwordReset() {
+        return new PasswordResetDto();
+    }
+
+
+    @GetMapping("/login")
+    public String login(Model model, String error, String logout) {
+        if (error != null)
+            model.addAttribute("error", "Your username or password is invalid.");
+
+        if (logout != null)
+            model.addAttribute("message", "You have been logged out successfully.");
+
+        return "login";
+    }
+
+
+    @RequestMapping(value="/forgot-password", method=RequestMethod.GET)
+    public String displayResetPassword(Model model) {
+       model.addAttribute("user", new User());
+        return "forgotPassword";
+    }
+
+    @RequestMapping(value="/forgot-password", method=RequestMethod.POST)
+    public String forgotUserPassword(@ModelAttribute("user") @Valid User user, BindingResult result) {
+
+        User existingUser = userRepository.findByUsername(user.getUsername());
+        if (existingUser != null) {
+            ConfirmationToken confirmationToken = new ConfirmationToken(existingUser);
+
+            confirmationTokenRepository.save(confirmationToken);
+
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+            mailMessage.setTo(existingUser.getUsername());
+            mailMessage.setSubject("Complete Password Reset!");
+            mailMessage.setFrom("test-email@gmail.com");
+            mailMessage.setText("To complete the password reset process, please click here: "
+                    + "http://localhost:8080/confirm-reset?token="+confirmationToken.getConfirmationToken());
+
+            emailSenderService.sendEmail(mailMessage);
+
+            return "successForgotPassword";
+
+        } else if (result.hasErrors()) {
+            return "forgotPassword";
+        } else if (user == null){
+            return "forgotPassword";
+        }
+        return "forgotPassword";
+    }
+
+
+    @RequestMapping(value="/confirm-reset", method= {RequestMethod.GET})
+    public String validateResetToken(Model model, @RequestParam("token")String confirmationToken) {
+        ConfirmationToken token = confirmationTokenRepository.findByConfirmationToken(confirmationToken);
+
+        if (token != null) {
+            User user = userRepository.findByUsername(token.getUser().getUsername());
+            user.getId();
+            model.addAttribute("user", user);
+        } else {
+            model.addAttribute("message", "The link is invalid or broken!");
+            model.addAttribute("error");
+        }
+        return "resetPassword";
+    }
+
+    @Transactional
+    @RequestMapping(value = "/confirm-reset", method = RequestMethod.POST)
+    public String resetUserPassword(@RequestParam("token")String confirmationToken, @Valid @ModelAttribute("passwordResetForm") PasswordResetDto form) {
+
+            ConfirmationToken token = confirmationTokenRepository.findByConfirmationToken(confirmationToken);
+            User user = token.getUser();
+
+            String updatedPassword = bCryptPasswordEncoder.encode(form.getPassword());
+
+            userService.updatePassword(updatedPassword, user.getId());
+
+
+        return "redirect:/login";
+    }
+
+
+    @RequestMapping(value="/logout", method = RequestMethod.GET)
+    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login?logout";
+    }
+
+
+    @GetMapping(value = "user/profile")
+    public String showinfo() {
+        return "userprofile";
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/views/userprofile.jsp
@@ -0,0 +1,40 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Profile</title>
+    <link href="../../resources/css/sb-admin.css" rel="stylesheet">
+</head>
+<body id="page-top">
+<form action="${pageContext.request.contextPath}/appLogout" method="POST">
+
+    <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
+
+        <a class="navbar-brand mr-1" href="/cost/all">ExpenseTracker</a>
+        <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
+            <i class="fas fa-bars"></i>
+        </button>
+
+        <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
+            <div class="input-group">
+            </div>
+        </form>
+
+        <!-- Navbar -->
+        <ul class="navbar-nav ml-auto ml-md-0">
+            <li class="userName">${pageContext.request.userPrincipal.name}</li>
+            <li class="nav-item dropdown no-arrow mx-1">
+                <a class="button" href="/user/profile">Profile</a>
+            </li>
+            <li class="nav-item dropdown no-arrow mx-1">
+                <input class = "button" type="submit" value="Logout"/>
+            </li>
+
+
+        </ul>
+
+    </nav>
+</form>
+<nav >Hello ${pageContext.request.userPrincipal.name}! </nav><br>
+<button class="submitForgot"><a href="/cost/all">Back to Main Page?</a></button>
+</body>
+</html>
