878415c01abeb7e79933ecc6cf5551c31476c394
Sonia Nocon <noconsonia@gmail.com>
2019-08-21 13:44:04 +0200

    repository

--- src/main/java/pl/repository/CostRepository.java
+++ src/main/java/pl/repository/CostRepository.java
@@ -1,11 +1,25 @@
 package pl.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pl.entity.Cost;
+
+import java.util.List;
 
 public interface CostRepository extends JpaRepository<Cost, Integer> {
 
     Cost findFirstById(int costId);
+
     Cost deleteById(int costId);
 
+    @Query(value = "select name, sum(amount) from cost inner join department where department.id = department_id group by department.name", nativeQuery = true)
+    List <Object[]> getGroupByDepartment();
+
+
+    @Query(value = "select name, sum(amount) from cost inner join client where client.id = client_id group by client.name", nativeQuery = true)
+    List <Object[]> getGroupByClient();
+
+    @Query(value = "select code, sum(amount) from cost inner join mpk where mpk.id = mpk_id group by mpk.code", nativeQuery = true)
+    List <Object[]> getGroupByMpk();
 }
+
--- src/main/java/pl/repository/UserRepository.java
+++ src/main/java/pl/repository/UserRepository.java
@@ -13,8 +13,8 @@ public interface UserRepository extends JpaRepository<User, Integer> {
      User findByUsername(String username);
 
 
-     @Query(value = "select * from user inner join user_role on user.id = users_id", nativeQuery = true)
-     List<User> findByUserWithRole();
+     @Query(value = "select user.id, username, name from user inner join user_role on user.id = users_id inner join role where roles_id = role.id", nativeQuery = true)
+     List<Object[]> findByUserWithRole();
 
      @Modifying
      @Query("update User u set u.password = :password where u.id = :id")
