0280838233d7996befd3e82ef90c4abbdc7d8a73
Sonia Nocon <noconsonia@gmail.com>
2019-08-22 19:07:25 +0200

    entity

--- src/main/java/pl/entity/User.java
+++ src/main/java/pl/entity/User.java
@@ -1,30 +1,94 @@
 package pl.entity;
 
-
-import org.hibernate.validator.constraints.NotBlank;
-
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "user")
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private int id;
 
-    @NotBlank
     @Column(unique = true)
-    private String login;
+    @Email(message = "Invalid e-mail address.")
+    @NotBlank(message = "This field is required.")
+    private String username;
 
-    @NotBlank
+    @Size(min = 4, message = "Please try one with at least 4 characters.")
     private String password;
 
-    @NotBlank
-    private String role;
+    @Transient
+    private String passwordConfirm;
 
-    @OneToOne
-    private UserRole userRole;
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Set<Role> roles = new HashSet<>();
 
+    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
+    private List<Cost> cost = new ArrayList<>();
+
+    private boolean isEnabled = true;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPasswordConfirm() {
+        return passwordConfirm;
+    }
+
+    public void setPasswordConfirm(String passwordConfirm) {
+        this.passwordConfirm = passwordConfirm;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        isEnabled = enabled;
+    }
+
+    public List<Cost> getCost() {
+        return cost;
+    }
+
+    public void setCost(List<Cost> cost) {
+        this.cost = cost;
+    }
 
 }
